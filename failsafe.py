688ab74189397d37a0d44a1564f14560ec886eac
Emeka Okonkwo <31867625+emo70@users.noreply.github.com>
2020-09-28 19:01:49 -0400

    Update failsafe.py

--- failsafe.py
+++ failsafe.py
@@ -2,8 +2,10 @@ import pyautogui
 import sys
 import win32api
 import win32con
+import time
+import datetime
 
-
+pyautogui.FailSafeException
 # When capslock is enabled exit program. If you want to use a keybind use the keyboard module
 def caps():
     if win32api.GetKeyState(win32con.VK_CAPITAL) == 1:
@@ -11,16 +13,27 @@ def caps():
         sys.exit()
 
 
-# Set up your bot how you want
-autobot = (
-    lambda: pyautogui.moveTo(197, 265, duration=1),
-    lambda: pyautogui.moveTo(297, 265, duration=1),
-    lambda: pyautogui.moveTo(297, 365, duration=1),
-    lambda: pyautogui.moveTo(197, 365, duration=1)
-)
+# Pause the program
+def pause():
+    while win32api.GetKeyState(win32con.VK_SCROLL) == 1:
+        print('paused...')
+        time.sleep(0.1)
 
-# Run the program
-while True:
-    for bot in autobot:
-        caps()
-        bot()
+
+# Kill the program at given time
+def kill():
+    now = datetime.datetime.now()
+    year = int(now.strftime("%Y"))
+    month = int(now.strftime("%m"))
+    day = int(now.strftime("%d"))
+    hour = int(now.strftime("%H"))
+    minute = int(now.strftime("%M"))
+    second = int(now.strftime("%S"))
+    check = datetime.datetime(
+        year=year, month=month, day=day, hour=hour, minute=minute, second=second)
+    kill = datetime.datetime(
+        year=year, month=month, day=day, hour=6, minute=minute, second=second)
+    if kill <= check:
+        print('End of program.')
+        sys.exit()
+
